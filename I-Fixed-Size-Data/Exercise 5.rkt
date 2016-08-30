1bdeef4810e73d17269cda9b3fdf0d3aef833c76
hussarka <balazs.nitsch@gmail.com>
2016-08-30 15:56:04 +0300

    Changed picture directory
    Put a picture from local

--- I-Fixed-Size-Data/Exercise 5.rkt
+++ I-Fixed-Size-Data/Exercise 5.rkt
@@ -1,5 +1,5 @@
 #lang htdp/bsl
 (require 2htdp/image)
 
-(define cat (bitmap/url "http://www.ccs.neu.edu/home/matthias/HtDP2e/cat1.png"))
+(define cat (bitmap/file "cat1.png"))
 (* (image-width cat) (image-height cat))
